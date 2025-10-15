12926bf51c19f975feb611b7368b72bd76529c5f
Anmol <anmolpaweriya0235@gmail.com>
2025-10-15 18:45:05 +0530

    updated api

--- src/CustomHooks/useSocket.tsx
+++ src/CustomHooks/useSocket.tsx
@@ -3,13 +3,20 @@ import { io, Socket } from "socket.io-client";
 
 const SocketContext = React.createContext<Socket | null>(null);
 
+export function useSocket() {
+  return useContext(SocketContext);
+}
 
-export function useSocket() { return useContext(SocketContext) }
-
-export default function SocketProvider({ children }: { children: React.ReactNode }) {
-    // const socket = io("http://localhost:8000", { autoConnect: false })
-    const socket = io("https://swiftsend.publicvm.com", { autoConnect: false })
-    return <SocketContext.Provider value={socket}>
-        {children}
-    </SocketContext.Provider>
+export default function SocketProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // const api = "https://swiftsend.publicvm.com";
+  // const api = "http://localhost:8000";
+  const api = "https://swiftsend.anmolpaweriya.online";
+  const socket = io(api, { autoConnect: false });
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
 }
